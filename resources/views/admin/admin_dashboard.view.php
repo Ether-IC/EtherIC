2bb80f5a12f2d77ddd11a2d311de2b1956e6b6ea
我不是3K官方！ <katharinaprice800@gmail.com>
2024-09-15 21:12:49 +0800

    Create admin_dashboard.view.php

---
+++ resources/views/admin/admin_dashboard.view.php
@@ -0,0 +1,29 @@
+<h2>Admin Dashboard</h2>
+
+<h3>Manage Users</h3>
+<ul>
+    <?php foreach ($users as $user): ?>
+        <li><?= $user['username'] ?> - Credits: <?= $user['credits'] ?> MB</li>
+    <?php endforeach; ?>
+</ul>
+
+<h3>Manage Packages</h3>
+<ul>
+    <?php foreach ($packages as $package): ?>
+        <li><?= $package['name'] ?> - <?= $package['traffic_limit'] ?> MB - $<?= $package['price'] ?></li>
+    <?php endforeach; ?>
+</ul>
+
+<h3>Orders</h3>
+<ul>
+    <?php foreach ($orders as $order): ?>
+        <li>User: <?= $order['username'] ?> - Package: <?= $order['package_name'] ?> - Status: <?= $order['status'] ?></li>
+    <?php endforeach; ?>
+</ul>
+
+<h3>Change Admin Password</h3>
+<form action="/change_admin_password.php" method="POST">
+    <label>New Password:</label>
+    <input type="password" name="new_password" required minlength="6" maxlength="255">
+    <button type="submit">Change Password</button>
+</form>
