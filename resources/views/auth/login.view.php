eb8792250eea6b7e5a1158f7fef444161539eafd
我不是3K官方！ <katharinaprice800@gmail.com>
2024-09-15 21:10:28 +0800

    Create login.view.php

---
+++ resources/views/auth/login.view.php
@@ -0,0 +1,7 @@
+<form action="/login.php" method="POST">
+    <label>Username:</label>
+    <input type="text" name="username" required>
+    <label>Password:</label>
+    <input type="password" name="password" required>
+    <button type="submit">Login</button>
+</form>
