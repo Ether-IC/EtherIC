bf8acea6971c7c5838f394966604d48816ef57b9
我不是3K官方！ <katharinaprice800@gmail.com>
2024-09-15 21:11:22 +0800

    Create register.view.php

---
+++ resources/views/auth/register.view.php
@@ -0,0 +1,7 @@
+<form action="/register.php" method="POST">
+    <label>Username:</label>
+    <input type="text" name="username" required minlength="4" maxlength="255">
+    <label>Password:</label>
+    <input type="password" name="password" required minlength="6" maxlength="255">
+    <button type="submit">Register</button>
+</form>
